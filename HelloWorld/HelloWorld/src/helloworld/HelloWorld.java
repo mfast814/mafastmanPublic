fa3825b1787976dacc7776de9fb53bc4631a1a69
mfast814 <mfast814@gmail.com>
2015-08-31 17:24:22 -0400

    First snapshot

---
+++ HelloWorld/HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+
+package helloworld;
+
+
+public class HelloWorld {
+
+    public static void printThing(String helloworld){
+        System.out.print(helloworld);
+    }
+   
+    public static void main(String[] args) {
+       printThing("Hello World!");
+    }
+
+   // private String helloworld;     
+}
